aab7fd9d43692b4f0486a4e116ca1ca9eccdb618
Antony <jns5@illinois.edu>
2022-02-23 09:52:54 +0100

    npm installed .env, finished the first transaction script

--- scripts/create-account.js
+++ scripts/create-account.js
@@ -2,12 +2,11 @@
 import algosdk from "algosdk"
 // Here we are creating an account - save the account address and mnemonic somwhere in your notes.
 // We will be using it several times to send transactions, deploy, and test our smart contracts
-// Make sure to fund your 
+// Make sure to fund your wallet with some test Algos!
 
 const myaccount = algosdk.generateAccount();
 console.log("Account created. Save address & Mnemonic!");
 console.log("Account Address = " + myaccount.addr);
 let account_mnemonic = algosdk.secretKeyToMnemonic(myaccount.sk);
 console.log("Account Mnemonic = "+ account_mnemonic);
-
-export default myaccount;
+console.log("Account Secret Key = " + myaccount.sk);
