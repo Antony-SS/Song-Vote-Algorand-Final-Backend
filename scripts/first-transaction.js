af93e9c399440678e8db342205e58737997ff915
Antony <jns5@illinois.edu>
2022-02-23 09:52:27 +0100

    npm installed .env, finished first transaction script

--- scripts/first-transaction.js
+++ scripts/first-transaction.js
@@ -1,35 +1,27 @@
-import myaccount from "./create-account.js";
 import dotenv from "dotenv";
+import algosdk from "algosdk";
 dotenv.config();
 
-// Great, your first account is funded, now let's try send a transaction!
 
-// First we have to initilize our sdk.  We need some way to interact with the test network and an easy way to do that
-// is throgh a third party, in this case that is purestake.
-// Make an account at https://developer.purestake.io/.  
-
-const algosdk = require('algosdk');
+let myaccount = algosdk.mnemonicToSecretKey(process.env.ACCOUNT_MNEMONIC);
 const baseServer = 'https://testnet-algorand.api.purestake.io/ps2'
 const port = '';
 const token = {
-    'X-API-Key': 'YOUR API KEY'
+    'X-API-Key': process.env.API_KEY
 }
 
-const algoclient = new algosdk.Algodv2(token, baseServer, port);
+const algodClient = new algosdk.Algodv2(token, baseServer, port); 
+// This variable is our client. It is the link between our code and the blockchain
 
 (async () => {
     try {
-        let params = await algoclient.getTransactionParams().do();
-        // comment out the next two lines to use suggested fee
-        // params.fee = algosdk.ALGORAND_MIN_TX_FEE;
-        // params.flatFee = true;
-
-        // reciever will be some random address, these tokens are worth nothing, this is just for testing purposes
-
+        let params = await algodClient.getTransactionParams().do();
+        
+        // reciever will be some random address.
         const receiver = "HZ57J3K46JIJXILONBBZOHX6BKPXEM2VVXNRFSUED6DKFD5ZD24PMJ3MVA";
         const enc = new TextEncoder();
         const note = enc.encode("My first transaction on Algo!");
-        let amount = 1000000; // equals 1 ALGO
+        let amount = 100000; // equals .1 ALGO
         let sender = myaccount.addr;
 
         let txn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
@@ -39,13 +31,29 @@ const algoclient = new algosdk.Algodv2(token, baseServer, port);
             node: note, 
             suggestedParams: params
         });
+
+        console.log("HERE3");
+
+        let accountInfo = await algodClient.accountInformation(myaccount.addr).do();
+        console.log("Account balance: %d microAlgos", accountInfo.amount);
+		
+        // sign transaction 
+
+        let signedTxn = txn.signTxn(myaccount.sk);
+        let txId = txn.txID().toString();
+        console.log("Signed transaction with txID: %s", txId);
+				
+        await algodClient.sendRawTransaction(signedTxn).do();
+
+        // Wait for confirmation
+        let confirmedTxn = await algosdk.waitForConfirmation(algodClient, txId, 4);
+        accountInfo = await algodClient.accountInformation(myaccount.addr).do();
+        console.log("Transaction Amount: %d microAlgos", confirmedTxn.txn.txn.amt);        
+        console.log("Transaction Fee: %d microAlgos", confirmedTxn.txn.txn.fee);
+        console.log("Account balance: %d microAlgos", accountInfo.amount);
+
     } catch (err) {
       console.error("Failed to get apps from the sdk", err);
       process.exit(1);
     }
   })()
-
-
-
-
-
