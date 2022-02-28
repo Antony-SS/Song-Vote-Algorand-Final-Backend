0a0a2aac2098af86c1cccca973f617c2cce5d7d4
Antony <jns5@illinois.edu>
2022-03-01 00:34:35 +0100

    figured out how to deploy using js sdk

--- contracts/counter.py
+++ contracts/counter.py
@@ -1,3 +1,4 @@
+import os
 from pyteal import *
 
 def approval_program():
@@ -17,26 +18,26 @@ def approval_program():
     scratchCount = ScratchVar(TealType.uint64)
 
     add = Seq(
-        # The initial `store` for the scratch var sets the value to 
-        # whatever is in the `Count` global state variable
         scratchCount.store(App.globalGet(Bytes("Count"))), 
-        # Increment the value stored in the scratch var 
-        # and update the global state variable 
         App.globalPut(Bytes("Count"), scratchCount.load() + Int(1)),
         Return(Int(1))
     )
 
     deduct = Seq(
-        # The initial `store` for the scratch var sets the value to 
-        # whatever is in the `Count` global state variable
         scratchCount.store(App.globalGet(Bytes("Count"))),
-        # Check if the value would be negative by decrementing 
         If(scratchCount.load() > Int(0),
-            # If the value is > 0, decrement the value stored 
-            # in the scratch var and update the global state variable
             App.globalPut(Bytes("Count"), scratchCount.load() - Int(1)),
         ),
         Return(Int(1))
+    )
+
+    handle_noop = Seq(
+        # First, fails immediately if this transaction is grouped with others
+        Assert(Global.group_size() == Int(1)), 
+        Cond(
+            [Txn.application_args[0] == Bytes("Add"), add], 
+            [Txn.application_args[0] == Bytes("Deduct"), deduct]
+        )
     )
 
     program = Cond(
@@ -54,4 +55,17 @@ def clear_state_program():
     # Mode.Application specifies that this is a smart contract
     return compileTeal(program, Mode.Application, version=5)
 
-print(approval_program())
+if __name__ == "__main__":
+
+    #path = os.path.dirname(os.path.abspath(__file__))
+
+    path = "./contracts/artifacts"
+    
+    with open(os.path.join(path, "counter_approval.teal"), 'w') as f:
+        f.write(approval_program())
+    
+    with open(os.path.join(path, "counter_clear.teal"), 'w') as f:
+        f.write(clear_state_program())
+    
+    export
+
